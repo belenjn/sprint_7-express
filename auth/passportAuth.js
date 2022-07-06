9d639fc52ea22f49e437fd3befb4952e4cbf5eca
Belen <belenjarabanavas31@gmail.com>
2022-07-06 11:57:33 +0200

    passportAuth updated

--- auth/passportAuth.js
+++ auth/passportAuth.js
@@ -5,6 +5,9 @@ var JwtStrategy = require("passport-jwt").Strategy;
 
 passport.use(
   new JwtStrategy(opts, function (jwt_payload, done) {
+    // jwt_payload is an object literal containing the decoded JWT payload.
+    //done is a passport error first callback accepting arguments done(error, user, info)
+
     if (username === "belen" && password === "1234") {
       return done(null, { username });
     } else {
