09bb9a270a75817d2454827408596b0abf636fc6
Belen <belenjarabanavas31@gmail.com>
2022-07-12 16:34:59 +0200

    db connected

--- db.js
+++ db.js
@@ -8,13 +8,5 @@ const connection = mysql.createConnection({
   database: db_mysql,
 });
 
-connection.connect();
-
-connection.query("SELECT 1 + 1 AS solution", (err, rows, fields) => {
-  if (err) throw err;
-
-  console.log("The solution is: ", rows[0].solution);
-});
-
-connection.end();
+module.exports = {connection}
 
