37aebde7c4cadcc2aade37b1e67ee6bb41fa4a2d
Belen <belenjarabanavas31@gmail.com>
2022-07-12 12:29:06 +0200

    Readme updated

--- db.js
+++ db.js
@@ -1,19 +1,20 @@
-const mysql = require('mysql')
-const { user_mysql, password_mysql, db_mysql } = require('./env');
+const mysql = require("mysql");
+const { user_mysql, password_mysql, db_mysql } = require("./env");
 
 const connection = mysql.createConnection({
-  host: 'localhost',
+  host: "localhost",
   user: user_mysql,
   password: password_mysql,
-  database: db_mysql
-})
+  database: db_mysql,
+});
 
-connection.connect()
+connection.connect();
 
-connection.query('SELECT 1 + 1 AS solution', (err, rows, fields) => {
-  if (err) throw err
+connection.query("SELECT 1 + 1 AS solution", (err, rows, fields) => {
+  if (err) throw err;
 
-  console.log('The solution is: ', rows[0].solution)
-})
+  console.log("The solution is: ", rows[0].solution);
+});
 
-connection.end()
+connection.end();
+
