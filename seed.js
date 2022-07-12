ebca1fabae811b6d9f5eca7022ca6d15cdfcd070
Belen <belenjarabanavas31@gmail.com>
2022-07-12 18:04:34 +0200

    fake data for users, contacts and rooms

--- seed.js
+++ seed.js
@@ -4,10 +4,10 @@ const { connection } = require("./db");
 connection.connect(function (err) {
   if (err) throw err;
 
-  /*
-  //Rellenar tabla users
+  
+  // Users
   const sqlUsers =
-    "INSERT INTO users (user_name, user_email, user_phone, start_date, occupation, status, photo, password) VALUES (?)";
+    "INSERT INTO users (user_name, user_email, user_phone, start_date, occupation, status, user_image, password) VALUES (?)";
   for (let i = 0; i < 10; i++) {
     let values = [
       faker.name.firstName() + faker.name.lastName(),
@@ -25,9 +25,11 @@ connection.connect(function (err) {
     });
   }
 
-  //Rellenar tabla contact
-  const sqlContact =
-    "INSERT INTO contact (contact_name, contact_email, contact_phone, contact_date, subject, comment, viewed, archived) VALUES (?)";
+  
+
+  // Contacts
+  const sqlContacts =
+    "INSERT INTO contacts (contact_name, contact_email, contact_phone, contact_date, subject, comment, viewed, archived) VALUES (?)";
   for (let i = 0; i < 10; i++) {
     let values = [
       faker.name.firstName() + faker.name.lastName(),
@@ -39,38 +41,43 @@ connection.connect(function (err) {
       faker.helpers.arrayElement([0, 1]),
       faker.helpers.arrayElement([0, 1]),
     ];
-    connection.query(sqlContact, [values], function (err, result) {
+    connection.query(sqlContacts, [values], function (err, result) {
       if (err) throw err;
       console.log("Number of records inserted: " + result.affectedRows);
     });
   }
 
-  */
-
-  //Rellenar tabla rooms
+  
+  // Rooms
   const sqlRooms =
-  "INSERT INTO rooms (room_number, bed_type, description, offer, price, discount, cancellation, amenities) VALUES (?)";
-for (let i = 1; i <= 10; i++) {
-  let values = [
-    i,
-    faker.helpers.arrayElement([
-      "single_bed",
-      "double_bed",
-      "double_superior",
-      "suite",
-    ]),
-    faker.lorem.sentence(10),
-    faker.helpers.arrayElement([0, 1]),
-    faker.finance.amount(50, 100, 0),
-    faker.finance.amount(0, 15, 0),
-    faker.lorem.sentence(10),
-    "TV",
-  ];
-  connection.query(sqlRooms, [values], function (err, result) {
-    if (err) throw err;
-    console.log("Number of records inserted: " + result.affectedRows);
-  });
-}
+    "INSERT INTO rooms (room_number, bed_type, description, offer, price, discount, cancellation, amenities) VALUES (?)";
+  for (let i = 1; i <= 10; i++) {
+    let values = [
+      i,
+      faker.helpers.arrayElement([
+        "single_bed",
+        "double_bed",
+        "double_superior",
+        "suite",
+      ]),
+      faker.lorem.sentence(10),
+      faker.helpers.arrayElement([0, 1]),
+      faker.finance.amount(50, 100, 0),
+      faker.finance.amount(0, 15, 0),
+      faker.lorem.sentence(10),
+      ["TV", "WIFI", "BATHROOM-KIT"]
+        .concat(
+          faker.helpers.arrayElements(["JACUZZI", "HAIR-DRYER", "MINIBAR"], 2)
+        )
+        .join(" "),
+    ];
+    connection.query(sqlRooms, [values], function (err, result) {
+      if (err) throw err;
+      console.log("Number of records inserted: " + result.affectedRows);
+    });
+  }
+
+  
   //Rellenar tabla rooms_images
   //Rellenar tabla bookings
 });
