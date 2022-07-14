c837e3dcacdb864d6d60de1c3c5ad065c2320e62
Belen <belenjarabanavas31@gmail.com>
2022-07-14 11:27:35 +0200

    roomsController done. All controllers finished.

--- controllers/roomsController.js
+++ controllers/roomsController.js
@@ -1,34 +1,74 @@
-let rooms = require("../data/rooms.json");
+const { connection } = require("../db");
 
 const getRooms = (req, res) => {
-  return res.json(rooms);
+  connection.query("SELECT * FROM rooms", (error, results, fields) => {
+    return res.json({ rooms: results });
+  });
 };
 
 const getRoom = (req, res) => {
-  const room = rooms.find((r) => String(r.id) === req.params.id);
-  return res.json(room);
+  const id = req.params.id;
+  connection.query(
+    "SELECT room_id, room_number, bed_type, description, offer, price, discount, cancellation, amenities, GROUP_CONCAT(url_image SEPARATOR ', ') AS images FROM rooms INNER JOIN room_images USING (room_id) WHERE room_id = ?; ",
+    [id],
+    (err, results) => {
+      return res.json({ room: results });
+    }
+  );
 };
 
 const deleteRoom = (req, res) => {
-  const roomId = rooms.find((room) => String(room.id) === req.params.id);
-  rooms.splice(roomId, 1);
-
-  return res.json({ success: true, message: "Room deleted" });
+  const id = req.params.id;
+  connection.query(
+    "DELETE FROM rooms WHERE room_id = ?",
+    [id],
+    (err, results) => {
+      return res.json({ success: true, message: "Room successfully deleted" });
+    }
+  );
 };
 
 const updateRoom = (req, res) => {
-  rooms.forEach((room, index) => {
-    if (room.id === req.params.id) {
-      return (rooms[index] = req.body);
+  const id = req.params.id;
+  connection.query(
+    "UPDATE rooms SET room_number = ?, bed_type = ?, description = ?, offer = ?, price = ?, discount = ?, cancellation = ?, amenities = ? WHERE room_id = ?",
+    [
+      req.body.room_number,
+      req.body.bed_type,
+      req.body.description,
+      req.body.offer,
+      req.body.price,
+      req.body.discount,
+      req.body.cancellation,
+      req.body.amenities,
+      id,
+    ],
+    (err, results) => {
+      return res.json({ success: true, message: "Room successfully updated" });
     }
-  });
-
-  return res.json({ success: true, message: "Room updated" });
+  );
 };
 
 const newRoom = (req, res) => {
-  rooms = [...rooms, req.body];
-  return res.json({ success: true, message: "New room created" });
+  const newRoom = [
+    req.body.room_number,
+    req.body.bed_type,
+    req.body.description,
+    req.body.offer,
+    req.body.price,
+    req.body.discount,
+    req.body.cancellation,
+    req.body.amenities,
+  ];
+  connection.query(
+    "INSERT INTO rooms (room_number, bed_type, description, offer, price, discount, cancellation, amenities) VALUES (?)",
+    [newRoom],
+    (err, results) => {
+      console.log(newRoom);
+      if (err) throw err;
+      return res.json({ success: true, message: "Room successfully added" });
+    }
+  );
 };
 
 module.exports = {
