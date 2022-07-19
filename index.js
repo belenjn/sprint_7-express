64e709737fb80ff8d32a1c72f508104aafb88560
Belen <belenjarabanavas31@gmail.com>
2022-07-19 09:21:24 +0200

    new field

---
+++ index.js
@@ -0,0 +1,6 @@
+require("./db");
+
+const Users = require("./models/Users");
+const Rooms = require("./models/Rooms");
+const Contacts = require("./models/Contacts");
+const Bookings = require("./models/Bookings");
