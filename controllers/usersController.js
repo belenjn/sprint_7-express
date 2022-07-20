949850409967f15d3dcf61af8fffb8fd6fb90671
Belen <belenjarabanavas31@gmail.com>
2022-07-20 15:24:42 +0200

    usersController updated and finished

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -7,42 +7,55 @@ const getUsers = async (req, res) => {
 };
 
 const getUser = async (req, res) => {
-  const user = await User.findOne({ _id: req.params.id });
-  return !user
-    ? res.status(404).json({ success: false, message: "User not found" })
-    : res.json(user);
+  try {
+    const user = await User.findOne({ _id: req.params.id });
+    return res.json(user);
+  } catch (error) {
+    res.status(404).json({ success: false, message: "User not found" });
+    console.log(error);
+  }
 };
 
 const deleteUser = async (req, res) => {
-  const user = await User.findOneAndDelete({ _id: req.params.id });
-  return !user
-    ? res.status(404).json({ success: false, message: "User not found" })
-    : res.json({ success: true, message: "User successfully deleted" });
+  try {
+    const user = await User.findOneAndDelete({ _id: req.params.id });
+    return res.json({ success: true, message: "User successfully deleted" });
+  } catch (error) {
+    res.status(404).json({ success: false, message: "User not found" });
+    console.log(error);
+  }
 };
 
 const updateUser = async (req, res) => {
-  const user = await User.findOneAndUpdate(
-    { _id: req.params.id },
-    {
-      user_name: req.body.user_name,
-      user_email: req.body.user_email,
-      user_phone: req.body.user_phone,
-      start_date: req.body.start_date,
-      occupation: req.body.occupation,
-      status: req.body.status,
-      user_image: req.body.user_image,
-      password: req.body.password,
-    }
-  );
-  return !user
-    ? res.status(404).json({ success: false, message: "User not found" })
-    : res.json({ success: true, message: "User successfully updated" });
+  try {
+    const user = await User.findOneAndUpdate(
+      { _id: req.params.id },
+      {
+        user_name: req.body.user_name,
+        user_email: req.body.user_email,
+        user_phone: req.body.user_phone,
+        start_date: req.body.start_date,
+        occupation: req.body.occupation,
+        status: req.body.status,
+        user_image: req.body.user_image,
+        password: req.body.password,
+      }
+    );
+    return res.json({ success: true, message: "User successfully updated" });
+  } catch (error) {
+    res.status(404).json({ success: false, message: "User not found" });
+    console.log(error);
+  }
 };
 
 const newUser = async (req, res) => {
-  const newUser = new User(req.body);
-  await newUser.save();
-  return res.json({ success: true, message: "User successfully added" });
+  try {
+    const newUser = new User(req.body);
+    await newUser.save();
+    return res.json({ success: true, message: "User successfully added" });
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 module.exports = {
